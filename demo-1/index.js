aa5f975e1a49d1aca987d8361e34ffc50490dfd9
Anamemem <anamememcyril540@gmail.com>
2021-06-30 14:47:13 +0100

    added nobles work

---
+++ demo-1/index.js
@@ -0,0 +1,10 @@
+const getNumber = (e)  =>{
+    let values = document.getElementById("values");
+    values.innerHTML += num
+}
+const equate = () =>{
+    let values = document.getElementById("values");
+    let values = document.getElementById("values");
+    value = values.innerHTML;
+    if(value[value.length -1] == ("+" | "-" | "/" | "*")){}
+}
